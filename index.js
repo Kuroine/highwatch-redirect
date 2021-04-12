056ae6bc77130ee605115563359188888d5d2e2d
Kevin <stonecold867@hotmail.com>
2021-04-12 14:42:34 +0800

    qol

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ module.exports = function HwRedirect(mod) {
   const highwatchRedeem = new Vec3(22205, 4870, 6191);
   const highwatchBanker = new Vec3(22438, 1605, 5857);
   const highwatchBush = new Vec3(19702,4052,6228);
+  const highwatchTG = new Vect3(21405, 4629, 6190);
   const bahaarTP = new Vec3(115023, 90044, 6377);
 
   let enabled = true;
+  let CDBlue, CDDP = 0;
+
+  mod.hook('S_START_COOLTIME_SKILL', 3 , event =>{
+    if(event.skill == 'A290100'){
+      CDBlue = Date.now() + event.cooldown;
+    }
+    if(event.skill == 'A300100'){
+      CDDP = Date.now() + event.cooldown;
+    }
+  });
 
   mod.hook('S_SPAWN_ME', 3, event => {
     if (enabled && mod.game.me.zone == 7031 && event.loc.equals(highwatchRedeem)) {
-      event.loc = highwatchBush;
+      if(CDBlue > Date.now() || CDDP > Date.now()){
+        event.loc = highwatchTG;
+      } 
+      else event.loc = highwatchBush;
     };
     if (mod.game.me.zone == 7004 && bahaarTP.dist3D(event.loc) <= 5) {
       event.loc = new Vec3(115321, 96917, 7196);
